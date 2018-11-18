6e6933b040779b3a0504d655c5309a3b4146cfb7
maxkvant <max.kvant@gmail.com>
2018-11-18 23:22:04 +0300

    moved metric form gpu to cpm

--- 89/homework/dcgan/dcgan.py
+++ 89/homework/dcgan/dcgan.py
@@ -14,12 +14,15 @@ class DCGenerator(nn.Module):
             nn.BatchNorm2d(channels_inside),
             nn.Upsample(scale_factor=2),
             nn.Conv2d(channels_inside, channels_inside, kernel_size=3, stride=1, padding=1),
+
             nn.BatchNorm2d(channels_inside, 0.8),
             nn.LeakyReLU(0.2, inplace=True),
             nn.Upsample(scale_factor=2),
+
             nn.Conv2d(channels_inside, channels_inside // 2, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(channels_inside // 2, 0.8),
             nn.Upsample(scale_factor=2),
+
             nn.LeakyReLU(0.2, inplace=True),
             nn.Conv2d(channels_inside // 2, out_channels, kernel_size=3, stride=1, padding=1),
             nn.Tanh()
@@ -29,7 +32,6 @@ class DCGenerator(nn.Module):
         out = self.l1(z.view(-1, self.latent_size))
         out = out.view(out.shape[0], self.layers, self.init_size, self.init_size)
         img = self.conv_blocks(out)
-        print("gen(z).size = {}".format(img.size()))
         return img
 
 
@@ -38,8 +40,6 @@ class DCDiscriminator(nn.Module):
         super(DCDiscriminator, self).__init__()
 
         self.img_size = img_size
-        print("self.img_size = {}".format(img_size))
-
         self.conv_layers = nn.Sequential(
             nn.Conv2d(in_channels, channels_inside, kernel_size=4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(channels_inside),
@@ -59,7 +59,5 @@ class DCDiscriminator(nn.Module):
         )
 
     def forward(self, img):
-        print("img_size = {}".format(img.size()))
         out = self.conv_layers(img)
-        print("out_size = {}".format(out.size()))
         return self.activation(out)
